e93b6ae201787b3b338121d0c95829d79bb76479
ChristianA <christiand.apostol@outlook.com>
2022-07-06 17:14:01 -0400

    javascript functionality

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// Call functions when the site starts
+window.onload = () => {
+   createMarker();
+}
+
+// Creates AFrame elements to render a mushroom cloud at the specified marker
+function createMarker() {
+    // Find a-scene
+    let scene = document.querySelector('a-scene');
+    
+    // Create Marker
+    let marker = document.createElement('a-marker');
+    marker.setAttribute('type', 'pattern');
+    marker.setAttribute('preset', 'custom');
+    marker.setAttribute('url', '/assets/mushroom-marker.patt')
+
+    // Create GLTF model
+    let model = document.createElement('a-entity');
+    model.setAttribute('position', '0 0 0');
+    model.setAttribute('scale', '0.005 0.005 0.005');
+    model.setAttribute('rotation', '0 90 -90'); // For when Marker is veritcal
+    model.setAttribute('gltf-model','/assets/mushroom_cloud/mushroomRealistic.glb');
+
+    // Add elements to scene
+    marker.appendChild(model);
+    scene.appendChild(marker);
+}
