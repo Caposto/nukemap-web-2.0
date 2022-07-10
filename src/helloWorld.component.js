e5a9565862574815d3996536825354edea95fdf9
ChristianA <christiand.apostol@outlook.com>
2022-07-10 14:39:03 -0400

    schema in components

--- src/helloWorld.component.js
+++ src/helloWorld.component.js
@@ -3,3 +3,13 @@ AFRAME.registerComponent('hello-world', {
         console.log("Hello Component World")
     }
 })
+
+AFRAME.registerComponent('log', {
+    schema: {
+        message: {type: 'string', default: 'Hello Component World'}
+    },
+
+    init: function() {
+        console.log(this.data.message)
+    }
+})
