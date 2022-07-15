b7ef73cf282a30da94bbd4e7449328915ebf6307
ChristianA <christiand.apostol@outlook.com>
2022-07-15 15:42:00 -0400

    3D mapbox with CDN

--- src/map.js
+++ src/map.js
@@ -2,11 +2,55 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 
-const mapbox_key = process.env.MAPBOX_API_KEY
+console.log(process.env.MAPBOX_API_KEY)
+mapboxgl.accessToken = process.env.MAPBOX_API_KEY
 
 const map = new mapboxgl.Map({
-    container: 'map', // HTML element
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-74.5, 40],
+    zoom: 9, 
+    pitch: 45,
+    bearing: -17.6,
+    antialias: true, 
 })
+
+map.on('load', () => {
+    const layers = map.getStyle().layers;
+    const labelLayerId = layers.find(
+        (layer) => layer.type === "symbol" && layer.layout['text-field']).id
+
+    map.addLayer({
+        'id': 'add-3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                15,
+                0,
+                15.05,
+                ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                15,
+                0,
+                15.05,
+                ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+        }
+    },
+    labelLayerId
+    )
+})
+
