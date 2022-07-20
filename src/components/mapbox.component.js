76ec320bccbf0cb89f41b7e12730a8cdd16b3b9f
ChristianA <christiand.apostol@outlook.com>
2022-07-19 22:12:46 -0400

    ar map nesting

--- src/components/mapbox.component.js
+++ src/components/mapbox.component.js
@@ -1,14 +1,18 @@
 // import mushroomMarker from './assets/mushroom-marker.patt'
 // import mapboxgl from 'mapbox-gl'
 
+import * as THREE from 'three'
 import nyc_map from '../assets/nyc.jpg'
 import mushroomMarker from '../assets/mushroom-marker.patt'
+import mapboxgl from 'mapbox-gl'
 
+// Container A-Frame Scene
 const scene = document.querySelector('a-scene')
 
 window.onload = () => {
-    // registerMapComponent
-    createImageAtMarker()
+    // registerMapComponent()
+    createMapAtMarker()
+    // createImageAtMarker()
 }
 
 function registerMapComponent() {
@@ -18,37 +22,24 @@ function registerMapComponent() {
             // Default LatLng Set to NYC
             latitude : {type: 'number', default: 40.7128},
             longitude : {type: 'number', default: 74.0060},
-            zoom: {type: 'number', default: 9}
         },
         init: function () {
-            // const accessToken = process.env.MAPBOX_API_KEY
-            const el = this.el
-            // el.setObject3D('mesh', this.map)
+            // Map Properties
+            const lat = this.data.latitude
+            const lng = this.data.lng
+            const center = [lat, lng]
+            const zoom = 12
+            const style = "mapbox://styles/mapbox/streets-v11"
+            const container_name = "a-entity"
+            let access_key = process.env.MAPBOX_API_KEY
 
-            this.map = this.createMap()
 
-            const mapLat = this.data.latitude
-            const mapLng = this.data.longitude
-            console.log("Map!" + "Long: " + this.data.longitude + " Lat: " + this.data.latitude)
         },
-        createMap: function () {
-            const map = new mapboxgl.Map({
-                container: 'map', // container ID
-                style: 'mapbox://styles/mapbox/streets-v11', // style URL
-                center: [this.data.longitude, this.data.latitude], // starting position [lng, lat]
-                zoom: this.data.zoom, // starting zoom
-            });
-
-            return map
-        }
 })
 }
 
 // FIXME: Change attributes to render map on the marker instead of mushroom cloud
 function createMapAtMarker() {
-    // Find a-scene
-    // let scene = document.querySelector('a-scene');
-    
     // Create Marker
     let marker = document.createElement('a-marker');
     marker.setAttribute('type', 'pattern');
@@ -56,12 +47,30 @@ function createMapAtMarker() {
     marker.setAttribute('url', mushroomMarker)
 
     // Add map component to scene
-    let map = document.createElement('a-entity');
-    map.setAttribute('map-image', ''); 
+    // let map_container = document.createElement('a-entity');
+    // map.setAttribute('mapbox-map', ''); 
+
+    let map_container = document.createElement('a-entity')
+    map_container.setAttribute('id', 'map')
+
+    marker.appendChild(map_container);
+    scene.appendChild(marker);
+
+    mapboxgl.accessToken = process.env.MAPBOX_API_KEY
+
+    const map = new mapboxgl.Map({
+        container: 'map', 
+        style: 'mapbox://styles/mapbox/streets-v11', 
+        center: [-74.5, 40], 
+        zoom: 9,
+        projection: 'globe' 
+    });
+
+    map.on('style.load', () => { map.setFog({}); });
 
     // Add elements to scene
-    marker.appendChild(map);
-    scene.appendChild(marker);
+    // marker.appendChild(map_container);
+    // scene.appendChild(marker);
 }
 
 // Function for creating map image
