cdfa094a4c3dace7e8bba8e795c1762f8e046b41
ChristianA <christiand.apostol@outlook.com>
2022-07-28 10:27:15 -0400

    basic threejs cloud

--- src/entries/cloud.js
+++ src/entries/cloud.js
@@ -2,26 +2,26 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-/**
- * Base
- */
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+/** Object */
+const baseGeometry = new THREE.CylinderGeometry(1, 1, 5)
+const baseMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const baseMesh = new THREE.Mesh(baseGeometry, baseMaterial)
 
-/**
- * Sizes
- */
+const topGeometry = new THREE.TorusGeometry(2, 0.8)
+const topMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff })
+const topMesh = new THREE.Mesh(topGeometry, topMaterial)
+
+topMesh.rotation.x = 1.5
+topMesh.position.y = 2
+
+scene.add(baseMesh)
+scene.add(topMesh)
+
+/** Sizes */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -62,30 +62,23 @@ window.addEventListener('dblclick', () => {
     }
 })
 
-/**
- * Camera
- */
-// Base camera
+/** Camera */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.z = 5
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+/** Renderer */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+/** Animation Loop */
 const clock = new THREE.Clock()
 
 const tick = () =>
