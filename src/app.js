f2aff454a48f8e7c5741401be54b5b790c7ec476
ChristianA <christiand.apostol@outlook.com>
2022-07-30 16:56:09 -0400

    add nyc_model to ar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import mushroomMarker from './assets/mushroom-marker.patt'
 import mushroomModel from './assets/mushroom_cloud/mushroomRealistic.glb'
-// import nycModel from './assets/nyc/nyc.glb'
+import nycModel from './assets/nyc/nyc.glb'
 
 // Creates AFrame elements to render a mushroom cloud at the specified marker
 function createMarker() {
@@ -15,7 +15,7 @@ function createMarker() {
 
     // Create GLTF model
     let model = document.createElement('a-entity');
-    model.setAttribute('gltf-model', mushroomModel);
+    model.setAttribute('gltf-model', nycModel);
     model.setAttribute('position', '0 0 0');
     model.setAttribute('scale', '0.001 0.001 0.001');
     model.setAttribute('rotation', '0 90 -90'); // For when Marker is veritcal
