f0897bbb12d60119d9582ff38158a6dbe18ec1e7
ChristianA <christiand.apostol@outlook.com>
2022-07-30 16:41:11 -0400

    reset back to commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import mushroomMarker from './assets/mushroom-marker.patt'
 import mushroomModel from './assets/mushroom_cloud/mushroomRealistic.glb'
+// import nycModel from './assets/nyc/nyc.glb'
 
 // Creates AFrame elements to render a mushroom cloud at the specified marker
 function createMarker() {
@@ -16,7 +17,7 @@ function createMarker() {
     let model = document.createElement('a-entity');
     model.setAttribute('gltf-model', mushroomModel);
     model.setAttribute('position', '0 0 0');
-    model.setAttribute('scale', '0.005 0.005 0.005');
+    model.setAttribute('scale', '0.001 0.001 0.001');
     model.setAttribute('rotation', '0 90 -90'); // For when Marker is veritcal
     model.setAttribute('class', 'clickable'); 
     model.setAttribute('gesture-handler', 'minScale: 0.005, maxScale: 0.01'); // For touch events
