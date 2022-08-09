c3194636f23fc80188c0d45e352f4b2f15cd1acf
ChristianA <christiand.apostol@outlook.com>
2022-08-09 19:49:31 -0400

    basic scene for tabletop

--- src/entries/table.js
+++ src/entries/table.js
@@ -1,9 +1,3 @@
 /**
  * Tabletop prototype with Hit-Testing (Maybe Anchoring) using WebXR, ThreeJS, A-Frame
  */
-
-import registerHideComponent from "../components/hide.component";
-
-window.onload = () => {
-    registerHideComponent()
-}
