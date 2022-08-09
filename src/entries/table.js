09cb64c9b52ac3e0f7894babc29dcf1c0f4459d6
ChristianA <christiand.apostol@outlook.com>
2022-08-09 19:00:22 -0400

    added hide in ar component

--- src/entries/table.js
+++ src/entries/table.js
@@ -1,3 +1,9 @@
 /**
  * Tabletop prototype with Hit-Testing (Maybe Anchoring) using WebXR, ThreeJS, A-Frame
  */
+
+import registerHideComponent from "../components/hide.component";
+
+window.onload = () => {
+    registerHideComponent()
+}
