08c0cbee32301a6e9d41c5e8c904b9177c8a127c
ChristianA <christiand.apostol@outlook.com>
2022-08-10 13:51:18 -0400

    hit test working

--- src/entries/table.js
+++ src/entries/table.js
@@ -1,3 +1,6 @@
 /**
  * Tabletop prototype with Hit-Testing (Maybe Anchoring) using WebXR, ThreeJS, A-Frame
  */
+import registerHitTestComponents from "../components/tabletop-ar-components";
+
+registerHitTestComponents()
