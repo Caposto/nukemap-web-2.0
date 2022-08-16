c775dc0e5da515c82743aa60549f6f953c4abccc
ChristianA <christiand.apostol@outlook.com>
2022-08-16 17:51:59 -0400

    added nick's mushroom cloud (no animations)

--- src/entries/table.js
+++ src/entries/table.js
@@ -4,51 +4,42 @@
 import nyc_map from '../assets/nyc/NYC.glb'
 import bare_map from '../assets/nyc/bare_nyc.glb'
 import bare_map_draco from '../assets/nyc/bare_nyc_draco.glb'
-import mushroom_cloud from '../assets/mushroom_cloud/mushroomRealistic.glb'
+import mushroom_cloud from '../assets/mushroom_cloud/nuke_mushroom.glb'
 import registerArCursorComponent from '../components/cursor.component'
 import registerShadowComponent from '../components/shadow.component'
 import registerMainComponent from '../components/main.component'
 
-// Instead of creating the Map on window load, create just the reticle
-// When screen is pressed emit an event "pressed" that begins to load the model
-window.onload = () => {
-    registerArCursorComponent()
-    registerShadowComponent()
-    registerMainComponent()
+// Creates 3D Map of NYC and adds Mushroom Cloud over top
+async function createNukemapAR() {
+    let assets = await document.querySelector('a-assets')
 
-    let assets = document.querySelector('a-assets')
     let map_asset = document.createElement('a-asset-item')
     map_asset.setAttribute('id', 'city')
     map_asset.setAttribute('src', bare_map)
     assets.appendChild(map_asset)
 
-    let container = document.getElementById("my-objects")
+    let mushroom_asset = document.createElement('a-asset-item')
+    mushroom_asset.setAttribute('id', 'cloud')
+    mushroom_asset.setAttribute('src', mushroom_cloud)
+    assets.appendChild(mushroom_asset)
+
+    // Attach map asset to scene and set scale
+    let container = await document.getElementById("my-objects")
     let map = document.createElement("a-gltf-model")
     map.setAttribute('src', '#city')
-    map.setAttribute('scale', '0.0005 0.0005 0.0005')
+    map.setAttribute('scale', '0.001 0.001 0.001')
+
+    let cloud = document.createElement("a-gltf-model")
+    cloud.setAttribute('src', '#cloud')
+    cloud.setAttribute('scale', '50 50 50')
+    map.appendChild(cloud)
 
     container.appendChild(map)
 }
 
-// NOT IN USE
-function createReticle() {
-    let container = document.querySelector('a-scene')
-
-    let reticle = document.createElement('a-entity')
-
-    reticle.setAttribute('id', 'reticle')
-    let plane = document.createElement('a-plane')
-    plane.setAttribute('rotation', '-90 0 0')
-    plane.setAttribute('height', '0.2')
-    plane.setAttribute('width', '0.2')
-
-    reticle.appendChild(plane)
-    // reticle.setAttribute('gltf-model', nyc_map)
-    // reticle.setAttribute('scale', '0.0001 0.0001 0.0001')
-
-    let mushroom = document.createElement('a-entity')
-    mushroom.setAttribute('gltf-model', mushroom_cloud)
-    mushroom.setAttribute('scale', '0.003 0.005 0.003')
-
-    container.appendChild(reticle)
-}
+// Instead of creating the Map on window load, create just the reticle
+// When screen is pressed emit an event "pressed" that begins to load the model
+registerArCursorComponent()
+registerShadowComponent()
+registerMainComponent()
+createNukemapAR()
